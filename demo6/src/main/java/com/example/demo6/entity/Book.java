343acc1e93dcb49606c4b6dd4dba57e63bcb18a5
Phomai <90522812+phomaizq@users.noreply.github.com>
2023-05-23 15:05:16 +0700

    git

---
+++ demo6/src/main/java/com/example/demo6/entity/Book.java
@@ -0,0 +1,63 @@
+package com.example.demo6.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "book")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    @Column(name = "title")
+    private  String title;
+    @Column(name = "author")
+    private  String author;
+    @Column(name = "price")
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private  Category category;
+}
