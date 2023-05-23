388540f74c646d12ae4e4b9d00d91333bbef9607
Phomai <90522812+phomaizq@users.noreply.github.com>
2023-05-23 14:57:43 +0700

    git

--- demo6/src/main/java/com/example/demo6/repository/ICategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo6.repository;
-
-import com.example.demo6.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICategoryRepository extends JpaRepository<Category, Long> {
-}
